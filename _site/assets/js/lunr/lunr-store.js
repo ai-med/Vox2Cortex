f23ee45977a62aed58b0b3694bcf208c42d3b1e9
arickm <annemarie.rickmann@gmail.com>
2022-05-23 14:23:19 +0200

    removed vox2cortex folder

---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2022/05/23/welcome-to-jekyll.html",
+        "teaser": null
+      }]
